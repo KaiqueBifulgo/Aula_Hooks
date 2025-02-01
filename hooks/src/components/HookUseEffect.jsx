1b603425e85671730875e21e265cada691a3efd8
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-02-01 01:34:19 -0300

    cleanup do useEffect

--- hooks/src/components/HookUseEffect.jsx
+++ hooks/src/components/HookUseEffect.jsx
@@ -19,11 +19,32 @@ const HookUseEffect = () => {
         console.log("Só ira repetir uma vez")
     }, []);
 
+
+    // ARRAY COM VALOR
+    const [anotherNumber, setAnotherNumber] = useState(0);
+
+    useEffect (() => {
+        if (anotherNumber > 0) {
+            console.log("Executa apenas quando muda o anotherNumber")
+        }
+    }, [anotherNumber])
+
+    // CLEANUP do useEFFECT
+    useEffect (() => {
+        const timer = setTimeout(() => {
+            console.log("Hello-world");
+            setAnotherNumber(anotherNumber + 1);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [anotherNumber])
+
   return (
     <div>
         <h2>useEffect</h2>
         <p>Numero: {number}</p>
         <button onClick={changeSomething}>Mudar</button>
+        <p>Another Number: {anotherNumber}</p>
+        <button onClick={(() => setAnotherNumber(anotherNumber + 2))}>Mudar another</button>
     </div>
   )
 }
